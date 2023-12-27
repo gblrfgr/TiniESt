dcf77270221756fb7512d67b856cf784668a3fbc
Juniper Mills <47700808+gblrfgr@users.noreply.github.com>
2023-12-27 16:46:45 -0600

    Broke ground on engine.hpp

---
+++ tiniest/engine.hpp
@@ -0,0 +1,14 @@
+#ifndef TINIEST_ENGINE_HPP
+#define TINIEST_ENGINE_HPP
+
+#include <tiniest/lex.hpp>
+
+namespace es {
+
+class MinimalEngine {};
+
+class CompliantEngine : public MinimalEngine {};
+
+} // namespace es
+
+#endif // TINIEST_ENGINE_HPP
