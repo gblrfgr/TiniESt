ece6c7cf1fa675b0a5553910227ed21864a15895
Juniper Mills <47700808+gblrfgr@users.noreply.github.com>
2024-01-16 10:25:29 -0600

    Continued work, created ObjectHeader, ObjectHandle

--- tiniest/engine.hpp
+++ tiniest/engine.hpp
@@ -2,12 +2,29 @@
 #define TINIEST_ENGINE_HPP
 
 #include <cstdlib>
+#include <optional>
 #include <tiniest/lex.hpp>
 
 namespace es {
 
+class ObjectHeader {
+private:
+  std::uint32_t m_contents;
+
+public:
+  ObjectHeader() = default;
+};
+
 typedef union {
+  ObjectHeader header;
 } MemoryBlock;
+
+class ObjectHandle {
+private:
+  ObjectHeader *m_header;
+};
+
+enum class error_t { OUT_OF_MEMORY };
 
 class Engine {
 private:
@@ -22,6 +39,20 @@ public:
   Engine(Engine &&) = default;
   ~Engine() = default;
   Engine &operator=(Engine &original);
+
+  void execute(const char *code);
+  std::optional<ObjectHandle> resolve_identifier(const char *identifier_name);
+  error_t create_empty_object(ObjectHandle &object,
+                              const char *identifier_name);
+  error_t create_number(ObjectHandle &object, double value,
+                        const char *identifier_name);
+  error_t create_string(ObjectHandle &object, const char *value,
+                        const char *identifier_name);
+  error_t create_boolean(ObjectHandle &object, bool value,
+                         const char *identifier_name);
+  error_t create_null(ObjectHandle &object, const char *identifier_name);
+  error_t create_undefined(ObjectHandle &object, const char *identifier_name);
+  error_t create_function(ObjectHandle &object, const char *identifier_name);
 };
 
 } // namespace es
